f32e0a4f9173cf7e4740b93ea45030a84b1e06cc
Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>
2022-01-10 22:36:44 -0300

    .
    Signed-off-by: Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { TweetModule } from './tweet/tweet.module';
+import { SeguindoModule } from './seguindo/seguindo.module';
+import { SeguidoresModule } from './seguidores/seguidores.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { TweetModule } from './tweet/tweet.module';
+import { SeguindoModule } from './seguindo/seguindo.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { SeguidoresModule } from './seguidores/seguidores.module';
 
 @Module({
-  imports: [],
+  imports: [UsuarioModule, TweetModule, SeguindoModule, SeguidoresModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
